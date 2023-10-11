c27a39842cbe1c05ec0e59505219841a5fc8da28
dev-JG <ljg615@nate.com>
2023-10-11 18:17:03 +0900

    [project-001] h2 설정, 테이블 설계

---
+++ blog-api/src/main/resources/h2/data.sql
@@ -0,0 +1,11 @@
+
+insert into keyword_search_info(keyword, count, status, last_search_date)
+values ('과자', 10, 'NORMAL',now());
+insert into keyword_search_info(keyword, count, status, last_search_date)
+values ('오늘의추천', 20, 'NORMAL',now());
+insert into keyword_search_info(keyword, count, status, last_search_date)
+values ('제주도', 110, 'NORMAL',now());
+insert into keyword_search_info(keyword, count, status, last_search_date)
+values ('렌터카', 230, 'NORMAL',now());
+insert into keyword_search_info(keyword, count, status, last_search_date)
+values ('여행지', 1210, 'NORMAL',now());
