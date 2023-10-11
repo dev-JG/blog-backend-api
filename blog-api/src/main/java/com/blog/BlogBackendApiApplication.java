520e704f2ec13127648f5ae465756e2502f41ffa
dev-JG <ljg615@nate.com>
2023-10-11 13:20:46 +0900

    init project

---
+++ blog-api/src/main/java/com/blog/BlogBackendApiApplication.java
@@ -0,0 +1,12 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackendApiApplication.class, args);
+    }
+}
